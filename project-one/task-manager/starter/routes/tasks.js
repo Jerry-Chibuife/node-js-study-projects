a4511e7adf1f3b3e529a692b6b3ce9ad4d6f4304
Jerry Chibuife <chibuifejerryu2003@gmail.com>
2022-07-10 12:43:12 +0100

    started new project

--- project-one/task-manager/starter/routes/tasks.js
+++ project-one/task-manager/starter/routes/tasks.js
@@ -4,6 +4,6 @@ const router = express.Router()
 const {getTasks, addTask, retrieveTask, editTask, removeTask} = require('../services/taskService')
 
 router.route('/').get(getTasks).post(addTask)
-router.route('/:id').get(retrieveTask).put(editTask).delete(removeTask)
+router.route('/:id').get(retrieveTask).patch(editTask).delete(removeTask)
 
 module.exports = router
--- project-one/task-manager/starter/services/taskService.js
+++ project-one/task-manager/starter/services/taskService.js
@@ -1,34 +1,47 @@
 const Task = require('../models/Task')
+const asyncWrapper = require('../middleware/async')
+const {createCustomError} = require('../errors/custom_error')
 
-const getTasks = async (req, res) => {
-    try {
-        const tasks = await Task.find()
-        res.status(200).json({tasks})
-    } catch (error) {
-        res.status(500).json({msg:error})
+const getTasks = asyncWrapper(async (req, res) => {
+    const tasks = await Task.find()
+    res.status(200).json({tasks})
+})
+
+const addTask = asyncWrapper(async (req, res) => {
+    const task = await Task.create(req.body)
+    return res.status(201).json({ task })
+})
+
+const retrieveTask = asyncWrapper(async (req, res, next) => {
+        const {id:taskID} = req.params
+        const task = await Task.findOne({_id:taskID})
+        if (!task){
+            return next(createCustomError(`No task found with id: ${taskID}`, 404))
+        }
+        res.status(200).json({task})
+})
+
+const editTask = asyncWrapper(async (req, res, next) => {
+    const {id:taskID} = req.params
+    const editDetails = req.body
+    const task = await Task.findByIdAndUpdate({_id:taskID}, editDetails, {
+        new:true, runValidators:true
+    })
+    if (!task){
+        return next(createCustomError(`No task found with id: ${taskID}`, 404))
     }
-}
+    res.status(200).json({task})
 
-const addTask = async (req, res) => {
-    try{
-        const task = await Task.create(req.body)
-        return res.status(201).json({ task })
-    } catch (error) {
-        res.status(500).json({msg: error})
+})
+
+const removeTask = asyncWrapper(async (req, res, next) => {
+    const {id:taskID} = req.params
+    const task = await Task.findOneAndDelete({_id:taskID})
+    if (!task){
+        return next(createCustomError(`No task found with id: ${taskID}`, 404))
     }
-}
-
-const retrieveTask = (req, res) => {
-    res.json({id:req.params.id})
-}
-
-const editTask = (req, res) => {
-    res.send('update task')
-}
-
-const removeTask = (req, res) => {
-    res.send('delete task')
-}
+    res.status(200).json({task})
+})
 
 module.exports = {
     getTasks, addTask, retrieveTask, editTask, removeTask
